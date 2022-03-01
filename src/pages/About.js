cbac8ea018e627e17ec95ad6381c00e79587ab60
federico-46 <federicocorrado46@gmail.com>
2022-03-01 11:30:26 +0100

    change About-page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,17 @@ import React from "react";
 const About = () => {
   return (
     <section className="section about-section">
-      <h1 className="section-title">About</h1>
+      <h1 className="section-title">About Us</h1>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
-        doloremque non consequatur delectus eligendi incidunt soluta quis
-        distinctio ducimus repellendus voluptatibus quia minima esse, accusamus,
-        adipisci at similique! Et dolorum quae ullam similique expedita. Eius
-        natus voluptate distinctio doloremque quas.
+        Vegetarian recipes, excellent for those who have opted for a
+        vegetable-based diet, a vegetarian diet is also ideal for purifying your
+        body. Vegetarian cuisine excludes the use of meat of any species from
+        human nutrition but does not exclude foods of animal origin, such as
+        eggs, milk and its derivatives, which are suitable for all ages.
+        However, there is a "pure" vegetarianism that excludes, always and in
+        any case, also any products deriving from animals and therefore also
+        milk and dairy products, basing the diet exclusively on substances of
+        vegetable origin.
       </p>
     </section>
   );
